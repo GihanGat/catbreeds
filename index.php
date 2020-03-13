ba7090099aefe31052b383cc287d7e2c3be0b284
Gihan Maduranga <gihanmadurangasl@gmail.com>
2020-03-13 03:35:01 -0400

    version1.0.4
    db connection

--- index.php
+++ index.php
@@ -6,55 +6,63 @@ $intelligence = htmlentities($_POST["intelligence"]);
 $activity = htmlentities($_POST["activity"]);
 
 // Create the PDO object for CloudSQL MySQL.
-$dsn = "mysql:dbname=cats;unix_socket=/cloudsql/catbreeds:northamerica-northeast1:catbreeds";
+//$dsn = "mysql:dbname=cats;unix_socket=/cloudsql/catbreeds:northamerica-northeast1:catbreeds";
+$dsn = "/cloudsql/catbreeds:northamerica-northeast1:catbreeds;dbname=cats";
 $user = "root";
 $password = "oshi1984";
 //$hostname = 
 $dbname = "cats";
-$pdo = new PDO($dsn, $user, $password);
+try{
+    $pdo = new PDO($dsn, $user, $password);
 
-echo($pdo);
-// Create a new database connection
-//$dbConn = new PDO("mysql:host=$hostname;dbname=$dbname", $username, $password);
-// Create the database if it doesn't exist
-$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-$pdo->query('CREATE TABLE IF NOT EXISTS catbreeds ' .
-    '( `id` INT(5) NOT NULL ,  `breed_name` VARCHAR(100) NOT NULL ,  `friendliness` INT(5) NOT NULL , 
-     `playfulness` INT(5) NOT NULL ,  `activity` INT(5) NOT NULL ,  `intelligence` INT(5) NOT NULL ,
-         PRIMARY KEY  (`id`)) ENGINE = InnoDB');        
-// Add the PDO object to our Silex application.
-$app['pdo'] = $pdo;
-echo($pdo);
-// Insert a visit into the database.
-/** @var PDO $pdo */
-$pdo = $app['pdo'];
-if $breed_name != ''{
-    //$insert = $pdo->prepare('INSERT INTO catbreeds (id,breed_name,friendliness,playfulness,activity,intelligence) values (?,?,?,?,?,?)');
-    $insert = $pdo->prepare('INSERT INTO catbreeds (breed_name,friendliness,playfulness,activity,intelligence) values (?,?,?,?,?)');
-    $insert->execute(array($breed_name, $friendliness, $playfulness, $activity, $intelligence,));
-    // $insert->execute(array("2","test breed2",4,2,3,5));
-    // $insert->execute(array("3","test breed3",3,2,4,5));
-    // $insert->execute(array("4","test breed4",5,3,2,4));
+    echo($pdo);
+    // Create a new database connection
+    //$dbConn = new PDO("mysql:host=$hostname;dbname=$dbname", $username, $password);
+    // Create the database if it doesn't exist
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->query('CREATE TABLE IF NOT EXISTS catbreeds ' .
+        '( `id` INT(5) NOT NULL ,  `breed_name` VARCHAR(100) NOT NULL ,  `friendliness` INT(5) NOT NULL , 
+        `playfulness` INT(5) NOT NULL ,  `activity` INT(5) NOT NULL ,  `intelligence` INT(5) NOT NULL ,
+            PRIMARY KEY  (`id`)) ENGINE = InnoDB');        
+    // Add the PDO object to our Silex application.
+    //$app['pdo'] = $pdo;
+    //echo($pdo);
+    // Insert a visit into the database.
+    /** @var PDO $pdo */
+    //$pdo = $app['pdo'];
+    print_r($pdo);
+    if $breed_name != ''{
+        //$insert = $pdo->prepare('INSERT INTO catbreeds (id,breed_name,friendliness,playfulness,activity,intelligence) values (?,?,?,?,?,?)');
+        $insert = $pdo->prepare('INSERT INTO catbreeds (breed_name,friendliness,playfulness,activity,intelligence) values (?,?,?,?,?)');
+        $insert->execute(array($breed_name, $friendliness, $playfulness, $activity, $intelligence,));
+        // $insert->execute(array("2","test breed2",4,2,3,5));
+        // $insert->execute(array("3","test breed3",3,2,4,5));
+        // $insert->execute(array("4","test breed4",5,3,2,4));
+    }
+    $select = $pdo->prepare('SELECT * FROM catbreeds');
+    $select->execute();
+    //$result = $stmt->fetch();
+    $catBreeds = [];
+    while ($row = $select->fetch(PDO::FETCH_ASSOC)) {
+        //array_push($catBreeds, sprintf('Breed Name: %s   Friendliness: %s', $row['breed_name'],$row['friendliness']));
+        //array_push($catBreeds, $row['breed_name'] , $row['friendliness']);
+        //array_push($catBreeds,$catBreeds[$row['breed_name']] => $row['friendliness']);
+        $catBreeds[] = array('breed_name'=> $row['breed_name'], 'friendliness'=> $row['friendliness']); 
+    }
+    //return new Response(implode("\n", $catBreeds), 200,['Content-Type' => 'text/plain']);
+ 
+    $method = $_SERVER["REQUEST_METHOD"];
+
+    if($method == "GET"){
+        $json = json_encode($catBreeds);
+        header("content-type: application/json");
+        //echo($json);
+    }else{}
+    return new Response($json, 200,['Content-Type' => 'application/json']);
+    }
 }
-$select = $pdo->prepare('SELECT * FROM catbreeds');
-$select->execute();
-//$result = $stmt->fetch();
-$catBreeds = [];
-while ($row = $select->fetch(PDO::FETCH_ASSOC)) {
-    //array_push($catBreeds, sprintf('Breed Name: %s   Friendliness: %s', $row['breed_name'],$row['friendliness']));
-    //array_push($catBreeds, $row['breed_name'] , $row['friendliness']);
-    //array_push($catBreeds,$catBreeds[$row['breed_name']] => $row['friendliness']);
-    $catBreeds[] = array('breed_name'=> $row['breed_name'], 'friendliness'=> $row['friendliness']); 
+}catch(PDOException $error){
+    $error -> getMessage();
 }
-//return new Response(implode("\n", $catBreeds), 200,['Content-Type' => 'text/plain']);
-
-$method = $_SERVER["REQUEST_METHOD"];
-
-if($method == "GET"){
-    $json = json_encode($catBreeds);
-    header("content-type: application/json");
-    //echo($json);
-}else{}
-return new Response($json, 200,['Content-Type' => 'application/json']);
 ?>
 
